78116bd3941ab0b2a9c6bf16096b66100b2eb691
chapmanethan <chapmanetha@gmail.com>
2016-10-08 16:04:18 -0600

    Camera2dFollow fix

--- Hidden/Assets/Scripts/Camera2dFollow.js
+++ Hidden/Assets/Scripts/Camera2dFollow.js
@@ -1,24 +1,18 @@
 ﻿#pragma strict
 
-//Written by Ethan Chapman @Eth0sire October 8, 2015 for FLIP
+//Written by Ethan Chapman @Eth0sire October 8, 2015 - October 8, 2016
 
  private var cameraObj : GameObject;
  public var specificVector : Vector3;
  public var smoothSpeed : float;
-// var menu : GameObject;
-// var pm : PauseMenu;
+
  
  function Start () { 
    cameraObj = GameObject.FindGameObjectWithTag("MainCamera");
-  // menu = GameObject.FindGameObjectWithTag("coinsSysMaster");
-  // pm = menu.GetComponent(PauseMenu);
-
  }
  
  function Update () {
-	//if (pm.isDead == false) {
-  		specificVector = Vector3(transform.position.x, transform.position.y, cameraObj.transform.position.z);
-   		cameraObj.transform.position = Vector3.Lerp(cameraObj.transform.position, specificVector, smoothSpeed * Time.deltaTime);
-   		//}
+  	 specificVector = Vector3(transform.position.x, 0, cameraObj.transform.position.z);
+   	 cameraObj.transform.position = Vector3.Lerp(cameraObj.transform.position, specificVector, smoothSpeed * Time.deltaTime);
    
  }
