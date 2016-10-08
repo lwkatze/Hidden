1c5d29f5fa42ea09962bf83c7fadc6fcae77760b
lwkatze <isaac.k.martin@gmail.com>
2016-10-07 22:39:45 -0600

    Added Stuff

--- Hidden/Assets/Scripts/Characters/InteractionHandler.cs
+++ Hidden/Assets/Scripts/Characters/InteractionHandler.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyAI : MonoBehaviour {
+public class InteractionHandler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
