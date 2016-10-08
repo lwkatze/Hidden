682b965cc6cb23408fa60afe36553b0fed518e49
lwkatze <isaac.k.martin@gmail.com>
2016-10-07 21:38:43 -0600

    Initial Upload

---
+++ Hidden/Assets/PlayerSpecific/CharacterData.cs
@@ -0,0 +1,211 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace App.Game.Player
+{
+public class CharacterData : MonoBehaviour 
+	{
+		
+		public static CharacterData charaData;
+
+		#region Data
+
+		public Transform player;
+		public Transform spawn;
+		public List<Transform> spawns;
+		public Transform gndCheck;
+		public Transform startPlat;
+
+		[HideInInspector] public Transform lastPlat;
+		[HideInInspector] public Transform currentPlat;
+
+		/// <summary>
+		/// Is the player touching the ground?
+		/// </summary>
+		/// <value><c>true</c> if gnd bool; otherwise, <c>false</c>.</value>
+		[HideInInspector]public bool gndBool
+		{
+			get { return Physics2D.OverlapArea(gndCheck.position, new Vector2(gndCheck.position.x + 1f, gndCheck.position.y + 1f), gndMask) ; }
+		}
+
+		/// <summary>
+		/// Gives information on the raycast from distFromGnd
+		/// </summary>
+		[HideInInspector]public RaycastHit2D gndRayHit { get { return Physics2D.Raycast(player.position, Vector2.down, Mathf.Infinity, gndMask); } }
+
+		/// <summary>
+		/// The distance from ground, if nothing is hit then previous value returned again.
+		/// </summary>
+		/// <value>The dist from gnd.</value>
+		public float distFromGnd
+		{
+			get { return (gndRayHit.collider != null) ? gndRayHit.distance : prev_distFromGnd; }
+		}
+
+		/// <summary>
+		/// distFromGnd from previous frame
+		/// </summary>
+		private float prev_distFromGnd;
+
+		/// <summary>
+		/// This gives the y velocity scaled 1 to -1. 1 means it's yvelocity is positive, 0 means 0, 0 to -1 is percentage of terminal Velocity.
+		/// </summary>
+		[HideInInspector]public float yVelocityScaled
+		{
+			get{ return (rgbody.velocity.y > 0) ? 1f : -1 * rgbody.velocity.y/termVelocity;}
+		}
+
+		/// <summary>
+		/// Set the terminal Velocity of player
+		/// </summary>
+		public float termVelocity = -60f;
+
+		/// <summary>
+		/// The LayerMask of the ground
+		/// </summary>
+		public LayerMask gndMask;
+
+		/// <summary>
+		/// Radius of the collider checking if player is hitting ground
+		/// </summary>
+		public float gndRadius = 0.2f;
+
+		[HideInInspector] public Vector3 vOnLand;
+		public Rigidbody2D rgbody;
+		public Animator anim;
+		public Camera playerCam;
+		public float jumpForce = 7200f;
+		public float moveSpeed = 20f;
+		public float score;
+		public float dist_death = 30f;
+		public bool collideState;
+		public bool prevcollideState;
+		public bool obstaclehit = false;
+
+		/// <summary>
+		/// Battery's recharge rate per second
+		/// </summary>
+		/// <value>The b percent power.</value>
+		public float b_Recharge = 10f;
+
+		/// <summary>
+		/// Battery's Capacity
+		/// </summary>
+		public float b_MaxPower = 100f;
+		public float b_Power;
+		public float b_PercentPower
+		{
+			get{ return b_Power/b_MaxPower ; }
+		}
+			
+		#endregion
+
+		private void setValues()
+		{
+			
+		}
+
+		#region Unity_Methods
+		void Awake()
+		{
+			if(charaData == null)
+			{
+				charaData = this;
+			}
+			if(charaData != this)
+			{
+				Destroy(gameObject);
+			}
+		}
+
+		void Start()
+		{
+			currentPlat = startPlat;
+			b_Power = b_MaxPower;
+			setValues();
+		}
+
+		void Update()
+		{
+			if(lastPlat.position.y - player.position.y >= dist_death)
+			{
+				Reload();
+			}
+			battery();
+
+			prev_distFromGnd = distFromGnd;
+			//Debug.Log("YVelocity: " + rgbody.velocity.y);
+		}
+
+		void LateUpdate()
+		{
+			prevcollideState = collideState;
+		}
+			
+		void OnCollisionEnter2D(Collision2D col)
+		{
+			collideState = true;
+			if(currentPlat != null)
+				lastPlat = currentPlat;
+			
+			currentPlat = col.transform;
+
+			if(col.transform.gameObject.layer == gndMask)
+			{
+				vOnLand = rgbody.velocity;
+			}
+		}
+
+		void OnCollisionExit2D(Collision2D col)
+		{
+			collideState = false;
+		}
+
+		#endregion
+
+		#region Methods
+
+		public void Reload()
+		{
+			
+		}
+
+		private void resetValues()
+		{
+
+		}
+
+		/// <summary>
+		/// Subtracts the power and returns a float determining the effect.
+		/// </summary>
+		/// <returns>Percentage of effect 0 - 1.</returns>
+		/// <param name="amount">Amount.</param>
+		public float subtractPower(float amount)
+		{
+			float rtnValue = 1;
+			if(b_Power >= amount)
+			{
+				rtnValue = 1;
+				b_Power -= amount;
+			}
+			else if(b_Power < amount)
+			{
+				rtnValue = b_Power/amount;
+				b_Power = 0;
+			}
+			return rtnValue;
+		}
+
+		private void battery()
+		{
+			if(b_Power < b_MaxPower)
+				b_Power += b_Recharge * Time.deltaTime;
+		}
+
+		#endregion
+
+	}
+}
---
+++ Hidden/Assets/PlayerSpecific/PlayerMove.cs
@@ -0,0 +1,259 @@
+﻿/* This is the script that controls player movement.
+ * It access the character data singleton to refernce
+ * player attributes (health, state, etc)
+ */
+
+using UnityEngine;
+using System.Collections;
+
+namespace App.Game.Player
+{
+	public class PlayerMove : MonoBehaviour {
+
+
+		public static PlayerMove playerMove;
+
+		/// <summary>
+		/// Access to the Characterdata singleton
+		/// </summary>
+		/// <value>The data.</value>
+		private CharacterData data 
+		{
+			get { return CharacterData.charaData; }
+		}
+
+	#region Data
+
+		public float PowerPerJump = 25f;
+		private Vector2 moveVector;
+		private bool facingRight;
+		private float currentSpeed;
+		private float h_axis;
+		private float v_axis;
+		private bool isDesktop
+		{
+			get{ return (Application.isMobilePlatform) ? false : true; }
+		}
+		//jumping and falling 
+		private float jumpForce
+		{
+			get { return CharacterData.charaData.jumpForce ; }
+		}
+		private float groundRadius
+		{
+			get { return CharacterData.charaData.gndRadius ; }
+		}
+		private float distDeath
+		{
+			get{ return CharacterData.charaData.dist_death; }
+		}
+		private Transform groundCheck
+		{
+			get { return CharacterData.charaData.gndCheck ; }
+		}
+		private LayerMask WhatIsGround
+		{
+			get { return CharacterData.charaData.gndMask ; }
+		}
+		private bool grounded
+		{
+			get { return CharacterData.charaData.gndBool ; }
+		}
+		private bool prev_grounded;
+		private float prev_yVelocity;
+
+	#endregion
+
+	#region Unity_Methods
+
+		void Awake()
+		{
+			if(playerMove == null)
+			{
+				playerMove = this;
+			}
+			if(playerMove != this)
+			{
+				Destroy(gameObject);
+			}
+		}
+
+		void Update()
+		{
+			MoveUpdate();
+			Animations();
+		}
+
+	#endregion
+
+		void MoveUpdate ()
+		{
+			if(h_axis > 0 && !facingRight)
+				Flip();
+			if(h_axis < 0 && facingRight)
+				Flip();
+			
+			KeyboardMove();
+			moveVector = new Vector2(h_axis * data.moveSpeed, data.rgbody.velocity.y);
+
+			if(data.rgbody.velocity.y <= -60.0f)
+			{
+				moveVector = new Vector2(moveVector.x, -60.0f);
+			}
+
+			data.rgbody.velocity = moveVector;
+		}
+
+		void Animations()
+		{
+			data.anim.SetBool("Grounded", grounded);
+			data.anim.SetBool("PrevGrounded", prev_grounded);
+			data.anim.SetFloat("vSpeedPercent", prev_yVelocity);
+
+			if(grounded && !prev_grounded)
+			{
+				
+				data.anim.SetFloat("vSpeed", data.vOnLand.y);
+			}
+			else if(grounded && prev_grounded)
+			{
+				
+			}
+			else if(!grounded && prev_grounded)
+			{
+				
+			}
+			else if(!grounded && !prev_grounded)
+			{
+				
+			}
+			prev_yVelocity = data.yVelocityScaled;
+			prev_grounded = grounded;
+		}
+	#region Input
+
+		void KeyboardMove()
+		{
+			if(Input.GetButtonDown("Jump"))
+			{
+				this.SendMessage("Jump");
+			}
+			if(Input.GetButton("Horizontal"))
+			{
+				h_axis = Input.GetAxis("Horizontal");
+			}
+			if(Input.GetButtonUp("Horizontal"))
+			{
+				h_axis = 0;
+			}
+		}
+		void Move(float value)
+		{
+			Debug.Log("Move Called");
+			h_axis = value;
+		}
+		void StopMove(float value)
+		{
+			h_axis = value;
+		}
+		void Jump ()
+		{
+			data.anim.SetTrigger("Jump");
+			if(data.gndBool == true)
+			{
+				data.rgbody.AddForce(new Vector2(0, jumpForce));
+			}
+			else
+			{
+				data.rgbody.velocity = new Vector2(data.rgbody.velocity.x, 0);
+				data.rgbody.AddForce(new Vector2(0, data.subtractPower(PowerPerJump)*jumpForce));
+			}
+		}
+
+		[SerializeField] private float t_distance = 6.0f;	//total distance of teleport
+		[SerializeField] private int t_Cost = 20;			//max power required for each full teleport
+		[SerializeField] private float t_Power = 120f;			//this is the current amount of power in the battery (value seen is starting value)
+		[SerializeField] private int t_MaxPower = 120;		//maximum "charge" of teleport battery, recharges over time
+		[SerializeField] private int t_MinPower = 1;		//minimum charge required to teleport
+		[SerializeField] private float t_RechargeRate = 10f;	//rate at which t_Power recharges
+
+		public float t_scaleCost = 1;	//scales the cost variable, varies from 0 to 1
+		public float t_Percent;		//used by HUD to see percent of total charge
+
+		void Teleport()
+		{
+			Vector3 t_Vector;
+			//teleport
+			if (t_Power >= t_MinPower) //can still teleport if t_MaxPower < t_Power... 
+			{
+				t_Vector = new Vector3(t_distance * t_scaleCost * Input.GetAxis("TeleportHorizontal"), 0f, 0f); //horizontal
+				data.player.transform.position += t_Vector;
+				//change values
+				if(t_Power >= t_Cost)
+				{
+					t_scaleCost = t_Cost/t_Cost;
+					t_Power = t_Power - t_Cost;
+				}
+				else if(t_Power < t_Cost && t_Power > 0)
+				{
+					t_scaleCost = t_Power/t_Cost;
+					t_Power = t_Power - t_scaleCost*t_Cost;
+				}
+			}
+			if(Input.GetButtonDown ("TeleportVertical") && t_Power >= t_MinPower) //...just won't go as far
+			{
+				t_Vector = new Vector3(0f, t_distance * t_scaleCost * Input.GetAxis("TeleportVertical"), 0f); //vertical 
+				data.player.transform.position += t_Vector;
+				//change values
+				if(t_Power >= t_Cost)
+				{
+					t_scaleCost = t_Cost/t_Cost;
+					t_Power = t_Power - t_Cost;
+				}
+				else if(t_Power < t_Cost && t_Power > 0)
+				{
+					t_scaleCost = t_Power/t_Cost;
+					t_Power = t_Power - t_scaleCost*t_Cost;
+				}
+			}
+			if(t_Power >= t_Cost)
+			{
+				t_scaleCost = t_Cost/t_Cost;
+			}
+			else if(t_Power < t_Cost && t_Power > 0)
+			{
+				t_scaleCost = t_Power/t_Cost;
+			}
+			if(t_Power < 0)
+			{
+				t_Power = 0;		//Power can't drop below zero, can it? At least not for purposes of this game
+			}
+			//recharge t_Power slowly according to t_RechargeRate.  Change the variable's value accordingly.
+			if(t_Power < t_MaxPower)
+			{
+				t_Power += t_RechargeRate*Time.deltaTime;//this will adapt the recharge time to stay consistent even with varying frame rates
+			}
+			//calculate percent
+			t_Percent = t_Power / t_MaxPower;
+		}
+
+	#endregion
+
+	#region Other_Methods
+
+		void Flip()
+		{
+			facingRight = !facingRight;
+			Vector3 theScale = data.player.transform.localScale;
+			theScale.x *= -1;
+			data.player.transform.localScale = theScale;
+		}
+
+		public void overrideMove()
+		{
+			currentSpeed = h_axis = v_axis = 0;
+		}
+
+	#endregion
+	}
+}
