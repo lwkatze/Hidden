4bf5117612f7c125654ac2381cd7a3b73e51834e
lafsnb <ikingst7@gmail.com>
2016-10-08 11:37:20 -0600

    Added Start Screen

---
+++ Hidden/Assets/Scripts/MainMenu/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public void Update(){
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            Debug.Log("Key pressed");
+            Application.LoadLevel(1);
+        }
+    }
+
+}
