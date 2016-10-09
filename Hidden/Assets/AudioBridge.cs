1c8a98fcac10e671499eb2b2bb5e4ec2953e1a24
chapmanethan <chapmanetha@gmail.com>
2016-10-09 11:35:39 -0600

    Audio

---
+++ Hidden/Assets/AudioBridge.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioBridge : MonoBehaviour
+{
+
+    public AudioController AC;
+
+    void Start()
+    {
+        AC = Camera.main.gameObject.GetComponent<AudioController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        AC.TrigEnter(trig.gameObject);
+    }
+
+    void OnTriggerExit2D(Collider2D trig)
+    {
+        AC.TrigExit(trig.gameObject);
+    }
+}
